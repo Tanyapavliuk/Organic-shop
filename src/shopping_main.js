48519451d4dae1e7eef5e92f10419633dbb607f1
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-08-15 18:08:18 +0300

    add partieals, images, css

--- src/shopping_main.js
+++
@@ -1,7 +0,0 @@
-import './js/header';
-import './js/dark-theme';
-import './js/support';
-import './js/shopping_list/shopping';
-import './js/loader';
-import './js/scrollUp';
-import './js/shopping_list/pagination';
